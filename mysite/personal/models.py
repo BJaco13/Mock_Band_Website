b00e6698ebb083b201b71d32391ce4ae37eb376c
Jaco <jacobeukes13@gmail.com>
2022-12-31 15:11:16 +0200

    Added in doc strings for the models file

--- mysite/personal/models.py
+++ mysite/personal/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Shows(models.Model):
+    '''
+    This is the Model Database to store any data regarding any shows that the band has.
+    This is then used on the site to display to the user all the data.
+
+    :param Date: Shows the date the show is happening.
+    :type Date: DateField
+
+    :param Time: Shows the time the show is happening.
+    :type Time: TimeField
+
+    :param Location: Shows the location where the show is happening. 
+    :type Location: CharField
+    '''
     # Default behaviour - Django creates primary keys for you
     date = models.DateField('show date')
     time = models.TimeField('show time')
